3a6f179f509dd8433dfc62c347bc446fed8fada4
quilariobenjo <94890755+quilariobenjo@users.noreply.github.com>
2025-06-24 17:16:56 +0000

    feat: refactor Hero component to use useMemo for badges and lazy load AvatarSection

--- src/components/section/hero.tsx
+++ src/components/section/hero.tsx
@@ -1,15 +1,27 @@
 "use client"
 
-import React from "react"
-import Image from "next/image"
-import { AspectRatio } from "@/components/ui/aspect-ratio"
+import React, { Suspense, useMemo } from "react"
 import { TypographyP } from "../typography"
-import avatar from "../../app/avatar.jpeg"
 import { FadeText } from "../ui/fade-text-"
 import { CORE_TECHNOLOGIES } from "../tech"
 import { Badge } from "../ui/badge"
+const AvatarSection = React.lazy(() => import("./hero-avatar-section"))
 
 const Hero = () => {
+  const badges = useMemo(
+    () =>
+      CORE_TECHNOLOGIES.map((tech) => (
+        <Badge
+          key={tech.name}
+          variant="secondary"
+          className="text-xs font-medium"
+        >
+          {tech.name}
+        </Badge>
+      )),
+    []
+  )
+
   return (
     <section className="mb-12 flex flex-col">
       <div className="flex w-full flex-col-reverse items-center gap-4">
@@ -17,17 +29,7 @@ const Hero = () => {
           <h4 className="font-heading scroll-m-20 text-lg font-medium tracking-tight">
             Here are a few technologies that are my cup of tea ☕
           </h4>
-          <div className="flex flex-wrap items-center gap-2">
-            {CORE_TECHNOLOGIES.map((tech) => (
-              <Badge
-                key={tech.name}
-                variant="secondary"
-                className="text-xs font-medium"
-              >
-                {tech.name}
-              </Badge>
-            ))}
-          </div>
+          <div className="flex flex-wrap items-center gap-2">{badges}</div>
         </div>
         <div className="flex w-full flex-col">
           <h1 className="bg-gradient-stop from-foreground via-foreground to-foreground/30 font-heading mt-2 scroll-m-20 text-pretty bg-gradient-to-br via-30% bg-clip-text text-4xl font-extrabold tracking-tighter text-transparent md:text-5xl">
@@ -39,19 +41,9 @@ const Hero = () => {
             development and UX/UI design
           </TypographyP>
         </div>
-        <div className="relative w-full">
-          <div className="relative h-[125px] w-[125px] rounded-md p-1 shadow-md">
-            <AspectRatio className="bg-muted relative rounded-md">
-              <Image
-                src={avatar}
-                alt="Benjo Quilario"
-                fill
-                style={{ objectFit: "cover" }}
-                className="rounded-md shadow-lg"
-              />
-            </AspectRatio>
-          </div>
-        </div>
+        <Suspense fallback={null}>
+          <AvatarSection />
+        </Suspense>
       </div>
     </section>
   )
